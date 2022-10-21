6ae1b935f9392e2110e92f61d46a0cd4ef18ab23
Gratic <blairodu74@yahoo.fr>
2022-10-21 08:37:39 +0200

    feat: added stats

--- deep_learning_project/trainers.py
+++ deep_learning_project/trainers.py
@@ -2,12 +2,15 @@ import torch
 
 class BaseTrainer():
 
-    def __init__(self, model, loss_fn, optimizer):
+    def __init__(self, model, loss_fn, optimizer, ):
         self.model = model
         self.loss_fn = loss_fn
         self.optimizer = optimizer
+
+        self.reset_stats()
     
     def fit(self, train_loader, test_loader, epochs, device):
+        self.reset_stats()
         for t in range(epochs):
             print(f"Epoch {t+1}\n-------------------------------")
             self.train_loop(train_loader, device)
@@ -19,6 +22,9 @@ class BaseTrainer():
         self.model.train()
 
         size = len(dataloader.dataset)
+        num_batches = len(dataloader)
+        train_loss, correct = 0, 0
+
         for batch, (X, y) in enumerate(dataloader):
             X = X.to(device)
             y = y.to(device)
@@ -35,6 +41,17 @@ class BaseTrainer():
             if batch % 1024 == 0:
                 loss, current = loss.item(), batch * len(X)
                 print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
+
+            train_loss += loss.item()
+            correct += (pred.argmax(1) == y).type(torch.float).sum().item()
+
+        train_loss /= num_batches
+        correct /= size
+
+        self.stats['train_loss'].append(train_loss)
+        self.stats['train_accuracy'].append(correct)
+
+
 
 
     def test_loop(self, dataloader, device):
@@ -55,4 +72,19 @@ class BaseTrainer():
 
         test_loss /= num_batches
         correct /= size
+
+        self.stats['test_loss'].append(test_loss)
+        self.stats['test_accuracy'].append(correct)
+
         print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
+    
+    def reset_stats(self):
+        self.stats = {
+            "train_loss": [],
+            "train_accuracy": [],
+            "test_loss": [],
+            "test_accuracy": []
+            }
+
+    def get_stats(self):
+        return self.stats
