a8ff72c5607f8b5f44de73e9bad093248590f277
Gratic <blairodu74@yahoo.fr>
2022-10-26 15:21:12 +0200

    feat: added validation in training

--- deep_learning_project/trainers.py
+++ deep_learning_project/trainers.py
@@ -1,4 +1,5 @@
 from copy import deepcopy
+from pure_eval import Evaluator
 import torch
 import os
 from tqdm import tqdm
@@ -11,10 +12,15 @@ class BaseTrainer():
         self.optimizer = optimizer
         self.checkpoints_path = os.path.join(checkpoints_path, 'checkpoint')
 
-        self.best_model = None
-        self.best_epoch = None
-        self.best_loss = None
-        self.best_accuracy = 0
+        self.best_valid_model = None
+        self.best_valid_epoch = None
+        self.best_valid_loss = None
+        self.best_valid_accuracy = 0
+
+        self.best_test_model = None
+        self.best_test_epoch = None
+        self.best_test_loss = None
+        self.best_test_accuracy = 0
 
         self.reset_stats()
 
@@ -24,21 +30,31 @@ class BaseTrainer():
             self.save_checkpoint = True
             os.makedirs(self.checkpoints_path, exist_ok=True)
         
-    def fit(self, train_loader, test_loader, epochs, device):
+    def fit(self, train_loader, valid_loader, test_loader, epochs, device):
         self.reset_stats()
         self.model.train()
 
         for t in range(epochs):
 
             self.train_loop(train_loader, device)
-            self.test_loop(test_loader, device)
+            self.valid_loop(valid_loader, device)
+
+            if test_loader != None:
+                self.test_loop(test_loader, device)
             
 
-            if self.stats['test_accuracy'][-1] > self.best_accuracy:
-                self.best_accuracy = self.stats['test_accuracy'][-1]
-                self.best_epoch = t
-                self.best_loss = self.stats['test_loss'][-1]
-                self.best_model = deepcopy(self.model.state_dict())
+            if self.stats['valid_accuracy'][-1] > self.best_valid_accuracy:
+                self.best_valid_accuracy = self.stats['valid_accuracy'][-1]
+                self.best_valid_epoch = t
+                self.best_valid_loss = self.stats['valid_loss'][-1]
+                self.best_valid_model = deepcopy(self.model.state_dict())
+            
+            if test_loader != None:
+                if self.stats['test_accuracy'][-1] > self.best_test_accuracy:
+                    self.best_test_accuracy = self.stats['test_accuracy'][-1]
+                    self.best_test_epoch = t
+                    self.best_test_loss = self.stats['test_loss'][-1]
+                    self.best_test_model = deepcopy(self.model.state_dict())
 
             if self.save_checkpoint:
                 torch.save({
@@ -49,13 +65,25 @@ class BaseTrainer():
                     'accuracy' : self.stats['train_accuracy'][-1],
                     }, os.path.join(self.checkpoints_path, 'checkpoint_' + str(t) + '.pt'))
             
-            print("epoch {0} of {1}: train_loss: {2:.5f}, train_accuracy: {3:.2%}, test_loss: {4:.5f}, test_accuracy: {5:.2%}"
-                .format(t+1,
-                        epochs,
-                        self.stats['train_loss'][-1],
-                        self.stats['train_accuracy'][-1],
-                        self.stats['test_loss'][-1],
-                        self.stats['test_accuracy'][-1])
+            if test_loader == None:
+                print("epoch {0} of {1}: train_loss: {2:.5f}, train_accuracy: {3:.2%}, valid_loss: {4:.5f}, valid_accuracy: {5:.2%}"
+                    .format(t+1,
+                            epochs,
+                            self.stats['train_loss'][-1],
+                            self.stats['train_accuracy'][-1],
+                            self.stats['valid_loss'][-1],
+                            self.stats['valid_accuracy'][-1])
+            else:
+                print("epoch {0} of {1}: train_loss: {2:.5f}, train_accuracy: {3:.2%}, valid_loss: {4:.5f}, valid_accuracy: {5:.2%}, test_loss: {4:.5f}, test_accuracy: {5:.2%}"
+                    .format(t+1,
+                            epochs,
+                            self.stats['train_loss'][-1],
+                            self.stats['train_accuracy'][-1],
+                            self.stats['valid_loss'][-1],
+                            self.stats['valid_accuracy'][-1],
+                            self.stats['test_loss'][-1],
+                            self.stats['test_accuracy'][-1])
+
             )
 
     def train_loop(self, dataloader, device):
@@ -85,35 +113,64 @@ class BaseTrainer():
         self.stats['train_loss'].append(train_loss)
         self.stats['train_accuracy'].append(correct)
 
-    def test_loop(self, dataloader, device):
+    def evaluate_model(self, dataloader, device):
         size = len(dataloader.dataset)
         num_batches = len(dataloader)
-        test_loss, correct = 0, 0
+        loss, accuracy = 0, 0
 
         with torch.no_grad():
             for X, y in dataloader:
                 X = X.to(device)
                 y = y.to(device)
                 pred = self.model(X)
-                test_loss += self.loss_fn(pred, y).item()
-                correct += (pred.argmax(1) == y).type(torch.float).sum().item()
+                loss += self.loss_fn(pred, y).item()
+                accuracy += (pred.argmax(1) == y).type(torch.float).sum().item()
 
-        test_loss /= num_batches
-        correct /= size
+        loss /= num_batches
+        accuracy /= size
 
-        self.stats['test_loss'].append(test_loss)
+        return (loss, accuracy)
+
+
+    def valid_loop(self, dataloader, device):
+        loss, correct = self.evaluate_model(dataloader, device)
+
+        self.stats['valid_loss'].append(loss)
+        self.stats['valid_accuracy'].append(correct)
+    
+    def test_loop(self, dataloader, device):
+        loss, correct = self.evaluate_model(dataloader, device)
+
+        self.stats['test_loss'].append(loss)
         self.stats['test_accuracy'].append(correct)
     
     def reset_stats(self):
         self.stats = {
             "train_loss": [],
             "train_accuracy": [],
+            "valid_loss": [],
+            "valid_accuracy": [],
             "test_loss": [],
-            "test_accuracy": []
+            "test_accuracy": [],
             }
 
     def get_stats(self):
         return self.stats
 
     def get_best_model(self):
-        return (self.best_epoch, self.best_accuracy, self.best_loss, self.best_model)
+        return { 
+                    "valid":
+                    {
+                        "epoch": self.best_valid_epoch,
+                        "accuracy": self.best_valid_accuracy,
+                        "loss": self.best_valid_loss,
+                        "model": self.best_valid_model
+                    },
+                    "test":
+                    {
+                        "epoch": self.best_test_epoch,
+                        "accuracy": self.best_test_accuracy,
+                        "loss": self.best_test_loss,
+                        "model": self.best_test_model
+                    }
+                }
