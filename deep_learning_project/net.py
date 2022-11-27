2e91b008f085e39f7e58a112271988ad6efda680
Gratic <blairodu74@yahoo.fr>
2022-11-27 12:58:31 +0100

    more model

--- deep_learning_project/net.py
+++ deep_learning_project/net.py
@@ -70,3 +70,20 @@ class ThirdNeuralNetwork(nn.Module):
         x = F.relu(self.fc1(x))
         x = self.fc2(x)
         return x
+
+class FourthNeuralNetwork(nn.Module):
+    def __init__(self) -> None:
+        super(FourthNeuralNetwork, self).__init__()
+
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv1 = nn.Conv2d(1, 32, 3)
+        self.conv2 = nn.Conv2d(32, 64, 3)
+        self.fc1 = nn.Conv2d(64, 64, 7)
+        self.fc2 = nn.Conv2d(64, 2, 1)
+    
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
